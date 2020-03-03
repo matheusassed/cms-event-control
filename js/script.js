7c6c84853b0bfcee69318b14fb66a2d906b85178
Matheus Assed <matheusassed@gmail.com>
2020-03-03 19:11:09 -0300

    Create

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* 
+<div class="swiper-slide">
+  <div>
+    <div id="evento-xxxx">
+      <h3 class="event-date"></h3>
+      <p class="event-description"></p>
+      <button class="ver-mais"></button>
+    </div>
+  </div>
+</div>
+*/
+
